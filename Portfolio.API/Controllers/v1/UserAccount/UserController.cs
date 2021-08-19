045f4db840d4b50dea290aba99161fb5e81ba1b4
Mahendra Pandey <mpandey@qkly.io>
2021-08-19 23:21:19 +0545

    response token on login

--- Portfolio.API/Controllers/v1/UserAccount/UserController.cs
+++ Portfolio.API/Controllers/v1/UserAccount/UserController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Portfolio.Application.User.Commands.CreateUser;
 using Portfolio.Application.User.Commands.UserLogin;
 using Portfolio.Core.API.Controllers;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Portfolio.API.Controllers.v1.UserAccount
@@ -9,6 +14,8 @@ namespace Portfolio.API.Controllers.v1.UserAccount
     [Route("api/v1/[controller]")]
     public class UserController : BaseController
     {
+        private string SecretKey = "fmFGn5agHZkuG2N0e1zaEJIQtGVoNN5P";
+
         [HttpPost("register")]
         public async Task<ActionResult> Create(CreateUserCommand command)
         {
@@ -19,8 +26,33 @@ namespace Portfolio.API.Controllers.v1.UserAccount
         [HttpPost("login")]
         public async Task<ActionResult> Login(UserLoginCommand command)
         {
-            var result = await Mediator.Send(command);
-            return Ok();
+            var user = await Mediator.Send(command);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SecretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.Email, user.Email)
+                    }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            // return basic user info and authentication token
+            return Ok(new
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                Email = user.Email,
+                Token = tokenString
+            });
         }
     }
 }
--- Portfolio.Application/User/Commands/UserLogin/UserLoginCommand.cs
+++ Portfolio.Application/User/Commands/UserLogin/UserLoginCommand.cs
@@ -1,21 +1,24 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Portfolio.Domain.Models.User;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Portfolio.Application.User.Commands.UserLogin
 {
-    public class UserLoginCommand : IRequest<bool>
+    public class UserLoginCommand : IRequest<ApplicationUser>
     {
         public string Email { get; set; }
         public string Password { get; set; }
     }
 
-    public class UserLoginCommandHandler : IRequestHandler<UserLoginCommand, bool>
+    public class UserLoginCommandHandler : IRequestHandler<UserLoginCommand, ApplicationUser>
     {
         private UserManager<ApplicationUser> _userManager;
         private SignInManager<ApplicationUser> _signInManager;
@@ -26,15 +29,15 @@ namespace Portfolio.Application.User.Commands.UserLogin
             _signInManager = signInManager;
         }
 
-        public async Task<bool> Handle(UserLoginCommand request, CancellationToken cancellationToken)
+        public async Task<ApplicationUser> Handle(UserLoginCommand request, CancellationToken cancellationToken)
         {
             var appUser = await _userManager.FindByEmailAsync(request.Email);
             var result = await _signInManager.PasswordSignInAsync(appUser, request.Password, false, false);
             if (result.Succeeded)
             {
-                var test = result;
+                return appUser;
             }
-            return true;
+            return null;
         }
     }
 }
